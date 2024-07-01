b1a7fe24f454fe98d02ede8668bf6ba9ef6cd295
Ralph McCracken <ralph@mccracken.work>
2024-07-01 02:00:19 -0500

    mobile ux

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ hide_streamlit_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
+
+            .block-container
+            {
+                padding-top: 1rem;
+                padding-bottom: 0rem;
+                margin-top: 1rem;
+            }
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
